b46ec8951dce2fd076b10b9e621433d4b13303a9
Liliya Khalikova <tomohisaPi@yandex.ru>
2023-11-25 16:42:51 +0300

    add catGetFoodTest

--- src/test/java/CatTest.java
+++ src/test/java/CatTest.java
@@ -2,20 +2,25 @@ import com.example.Cat;
 import com.example.Feline;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
 
 import static org.junit.Assert.assertEquals;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CatTest {
-    @Mock
-    Feline mockFeline;
+    Feline feline = new Feline();
 
     @Test
     public void getSoundTest() {
-        Cat cat = new Cat(mockFeline);
+        Cat cat = new Cat(feline);
         assertEquals(cat.getSound(), "Мяу");
     }
 
+    @Test
+    public void getFoodTest() throws Exception {
+        Cat cat = new Cat(feline);
+        assertEquals(cat.getFood(), List.of("Животные", "Птицы", "Рыба"));
+    }
 }
